c424634d07d48f0d366f35f5b4a2bb81fd5ae870
developeruche <developeruche@gmail.com>
2025-04-28 18:04:22 +0100

    ci

--- poly/src/vpoly.rs
+++ poly/src/vpoly.rs
@@ -1,9 +1,12 @@
 //! This module contains the implementation of the virtual polynomial.
 //! A virtual polynomials is a Vector of MLEs having a combination relationship.
-use std::{fmt::{self, Debug, Formatter}, rc::Rc};
+use std::{
+    fmt::{self, Debug, Formatter},
+    rc::Rc,
+};
 
-use p3_field::Field;
 use crate::mle::MultilinearPoly;
+use p3_field::Field;
 
 pub struct VPoly<F: Field> {
     /// The MLEs that make up the virtual polynomial.
@@ -22,35 +25,47 @@ impl<F: Field + Debug> Debug for VPoly<F> {
             .field("mles", &self.mles)
             .field("max_degree", &self.max_degree)
             .field("num_vars", &self.num_vars)
-            .field("combine_fn", &"<function>")  // Display placeholder for the function
+            .field("combine_fn", &"<function>") // Display placeholder for the function
             .finish()
     }
 }
 
-
 impl<F: Field> VPoly<F> {
     /// Creates a new virtual polynomial from a vector of MLEs and a combination function.
-    pub fn new(mles: Vec<MultilinearPoly<F>>, max_degree: usize, num_vars: usize, combine_fn: Rc<dyn Fn(&[F]) -> F>) -> Self {
+    pub fn new(
+        mles: Vec<MultilinearPoly<F>>,
+        max_degree: usize,
+        num_vars: usize,
+        combine_fn: Rc<dyn Fn(&[F]) -> F>,
+    ) -> Self {
         // assert all MLEs have the same number of variables
-        assert!(mles.iter().all(|mle| mle.num_vars() == num_vars), "MLEs must have the same number of variables");
-        VPoly { mles, max_degree, num_vars, combine_fn }
+        assert!(
+            mles.iter().all(|mle| mle.num_vars() == num_vars),
+            "MLEs must have the same number of variables"
+        );
+        VPoly {
+            mles,
+            max_degree,
+            num_vars,
+            combine_fn,
+        }
     }
-    
+
     /// Poly max degree
     pub fn max_degree(&self) -> usize {
         self.max_degree
     }
-    
+
     /// Number of variables in the polynomial
     pub fn num_vars(&self) -> usize {
         self.num_vars
     }
-    
+
     /// Number of MLEs in the polynomial
     pub fn num_mles(&self) -> usize {
         self.mles.len()
     }
-    
+
     /// MLEs in the polynomial
     pub fn mles(&self) -> Vec<MultilinearPoly<F>> {
         self.mles.clone()
@@ -60,14 +75,22 @@ impl<F: Field> VPoly<F> {
 impl<F: Field> VPoly<F> {
     /// Evaluates the virtual polynomial at a given point.
     pub fn evaluate(&self, point: &[F]) -> F {
-        let values = self.mles.iter().map(|mle| mle.evaluate(point)).collect::<Vec<_>>();
+        let values = self
+            .mles
+            .iter()
+            .map(|mle| mle.evaluate(point))
+            .collect::<Vec<_>>();
         (self.combine_fn)(&values)
     }
-    
+
     /// Partial evaluation of the virtual polynomial at a given point.
     pub fn partial_evalute(&self, point: &[F]) -> Self {
-        let values = self.mles.iter().map(|mle| mle.partial_evalute(point)).collect::<Vec<_>>();
-        
+        let values = self
+            .mles
+            .iter()
+            .map(|mle| mle.partial_evalute(point))
+            .collect::<Vec<_>>();
+
         Self {
             mles: values,
             max_degree: self.max_degree,
@@ -77,21 +100,20 @@ impl<F: Field> VPoly<F> {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use p3_field::AbstractField;
     use p3_goldilocks::Goldilocks as F;
-    
+
     fn prod_combined_fn(values: &[F]) -> F {
         values[0] * values[1]
     }
-    
+
     fn combined_fn_1(values: &[F]) -> F {
-        (F::from_canonical_u64(2) * values[0] * values[1]) +  values[2]
+        (F::from_canonical_u64(2) * values[0] * values[1]) + values[2]
     }
-    
+
     fn f_abc() -> MultilinearPoly<F> {
         // f(a,b,c) = 2ab + 3bc;
         MultilinearPoly::new_from_vec(
@@ -102,7 +124,7 @@ mod tests {
                 .collect(),
         )
     }
-    
+
     #[test]
     #[should_panic = "MLEs must have the same number of variables"]
     fn test_varying_lenght() {
@@ -118,54 +140,64 @@ mod tests {
         let point = vec![F::from_canonical_u64(1), F::from_canonical_u64(2)];
         vpoly.partial_evalute(&point);
     }
-    
+
     #[test]
     fn test_meta_data_test() {
         let mles = vec![f_abc(), f_abc()];
         let vpoly = VPoly::new(mles, 1, 3, Rc::new(prod_combined_fn));
-        
-        
+
         assert_eq!(vpoly.num_vars(), 3);
         assert_eq!(vpoly.max_degree(), 1);
         assert_eq!(vpoly.num_mles(), 2);
     }
-    
+
     #[test]
     fn test_partial_evaluation() {
         let mles = vec![f_abc(), f_abc()];
         let vpoly = VPoly::new(mles, 1, 3, Rc::new(prod_combined_fn));
-        
+
         let point = vec![F::from_canonical_u64(4)];
-        let expected_mles = vec![MultilinearPoly::new_from_vec(
-            2,
-            vec![0, 0, 8, 11]
-                .into_iter()
-                .map(|n| F::from_canonical_u64(n))
-                .collect(),
-        ), MultilinearPoly::new_from_vec(
-            2,
-            vec![0, 0, 8, 11]
-                .into_iter()
-                .map(|n| F::from_canonical_u64(n))
-                .collect(),
-        )];
+        let expected_mles = vec![
+            MultilinearPoly::new_from_vec(
+                2,
+                vec![0, 0, 8, 11]
+                    .into_iter()
+                    .map(|n| F::from_canonical_u64(n))
+                    .collect(),
+            ),
+            MultilinearPoly::new_from_vec(
+                2,
+                vec![0, 0, 8, 11]
+                    .into_iter()
+                    .map(|n| F::from_canonical_u64(n))
+                    .collect(),
+            ),
+        ];
         assert_eq!(vpoly.partial_evalute(&point).mles(), expected_mles);
     }
-    
+
     #[test]
     fn test_eval() {
         let mles = vec![f_abc(), f_abc()];
         let vpoly = VPoly::new(mles, 2, 3, Rc::new(prod_combined_fn)); // combination => (a * b)
-        let points = vec![F::from_canonical_u64(1), F::from_canonical_u64(2), F::from_canonical_u64(3)];
+        let points = vec![
+            F::from_canonical_u64(1),
+            F::from_canonical_u64(2),
+            F::from_canonical_u64(3),
+        ];
         let expected_mles = F::from_canonical_u64(22 * 22);
         assert_eq!(vpoly.evaluate(&points), expected_mles);
     }
-    
+
     #[test]
     fn test_eval_1() {
         let mles = vec![f_abc(), f_abc(), f_abc()];
         let vpoly = VPoly::new(mles, 2, 3, Rc::new(combined_fn_1)); // combination => 2(a * b) + c
-        let points = vec![F::from_canonical_u64(1), F::from_canonical_u64(2), F::from_canonical_u64(3)];
+        let points = vec![
+            F::from_canonical_u64(1),
+            F::from_canonical_u64(2),
+            F::from_canonical_u64(3),
+        ];
         let expected_mles = F::from_canonical_u64(990);
         assert_eq!(vpoly.evaluate(&points), expected_mles);
     }
