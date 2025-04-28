5c93ee9d66efe523e364561ef5658cdc604cfe22
Wisdom Ogwu <iammadab@gmail.com>
2025-04-28 12:56:40 +0100

    add pub identifier to mle fns

--- poly/src/mle.rs
+++ poly/src/mle.rs
@@ -1,15 +1,15 @@
 use p3_field::Field;
 
-struct MLE<F: Field> {
+pub struct MLE<F: Field> {
     evaluations: Vec<F>,
 }
 
 impl<F: Field> MLE<F> {
-    fn partial_evalute(points: &[F]) -> F {
+    pub fn partial_evalute(points: &[F]) -> F {
         todo!()
     }
 
-    fn evaluate(points: &[F]) -> F {
+    pub fn evaluate(points: &[F]) -> F {
         todo!()
     }
 }
