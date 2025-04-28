8613c0a0af802e30446a157a05b714377634b2ef
developeruche <developeruche@gmail.com>
2025-04-28 13:24:37 +0100

    check-point

--- poly/src/vpoly.rs
+++ poly/src/vpoly.rs
@@ -3,7 +3,29 @@
 use p3_field::Field;
 use crate::mle::MLE;
 
-// TODO: added derived macros // #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct VPoly<F: Field> {
-    pub mles: Vec<MLE<F>>,
+    /// The MLEs that make up the virtual polynomial.
+    mles: Vec<MLE<F>>,
+    /// The function used to combine the values of the MLEs.
+    combine_fn: Box<dyn Fn(&[F]) -> F>,
+    /// max possible degree of the polynomial
+    max_degree: usize,
+}
+
+
+impl<F: Field> VPoly<F> {
+    /// Creates a new virtual polynomial from a vector of MLEs and a combination function.
+    pub fn new(mles: Vec<MLE<F>>, combine_fn: Box<dyn Fn(&[F]) -> F>, max_degree: usize) -> Self {
+        VPoly { mles, combine_fn, max_degree }
+    }
+}
+
+impl<F: Field> VPoly<F> {
+    /// Evaluates the virtual polynomial at a given point.
+    pub fn evaluate(&self, point: &[F]) -> F {
+        let values = self.mles.iter().map(|mle| mle.evaluate(point)).collect::<Vec<_>>();
+        (self.combine_fn)(&values)
+    }
+    
+    /// 
 }
