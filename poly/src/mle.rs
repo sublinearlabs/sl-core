b321cbc1bbcae984d78deb6d5c21fbeade12652b
Wisdom Ogwu <iammadab@gmail.com>
2025-04-28 13:50:27 +0100

    implement new fn for mle

--- poly/src/mle.rs
+++ poly/src/mle.rs
@@ -1,10 +1,22 @@
 use p3_field::Field;
 
 pub struct MLE<F: Field> {
+    /// The evaluations of the boolean hypercube {0,1}^n_vars
     evaluations: Vec<F>,
+    /// Number of variables
+    n_vars: usize,
 }
 
 impl<F: Field> MLE<F> {
+    pub fn new_from_vec(n_vars: usize, evaluations: Vec<F>) -> Self {
+        // assert that the number of variables matches the number of evaluations
+        assert_eq!(1 << n_vars, evaluations.len());
+        Self {
+            evaluations,
+            n_vars,
+        }
+    }
+
     pub fn partial_evalute(&self, points: &[F]) -> Self {
         todo!()
     }
