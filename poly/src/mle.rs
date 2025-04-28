7f1909f190ebb0ba828f52d2ba102dcf27776b5e
Wisdom Ogwu <iammadab@gmail.com>
2025-04-28 14:09:02 +0100

    impl partial evaluation

--- poly/src/mle.rs
+++ poly/src/mle.rs
@@ -18,7 +18,38 @@ impl<F: Field> MLE<F> {
     }
 
     pub fn partial_evalute(&self, points: &[F]) -> Self {
-        todo!()
+        // ensure we don't have more points than variables
+        assert!(points.len() <= self.n_vars);
+
+        let mut new_evaluations = self.evaluations.clone();
+
+        // for each partial point, fold the evaluations in half
+        let mut mid_point = new_evaluations.len() / 2;
+        for point in points {
+            for i in 0..mid_point {
+                let left = new_evaluations[i];
+                let right = new_evaluations[i + mid_point];
+                new_evaluations[i] = match point {
+                    a if a.is_zero() => left,
+                    a if a.is_one() => right,
+                    _ => {
+                        // linear interpolation
+                        // (1-r) * left + r * right
+                        // left - r.left + r.right
+                        // left - r (left - right)
+                        left - *point * (left - right)
+                    }
+                }
+            }
+            mid_point /= 2;
+        }
+
+        // truncate and return new polynomial
+        let n_vars = self.n_vars - points.len();
+        Self {
+            evaluations: new_evaluations[..(1 << n_vars)].to_vec(),
+            n_vars,
+        }
     }
 
     pub fn evaluate(&self, points: &[F]) -> F {
