ad8ad3f440732f3379bbfd588272735ef85370cd
Wisdom Ogwu <iammadab@gmail.com>
2025-04-29 10:52:03 +0100

    define the multilinear extension trait

--- poly/src/lib.rs
+++ poly/src/lib.rs
@@ -1,2 +1,9 @@
 pub mod mle;
 pub mod vpoly;
+
+pub trait MultilinearExtension<F> {
+    fn evaluate(&self, point: &[F]) -> F;
+    fn partial_evaluate(&self, point: &[F]) -> Self;
+    fn max_degree(&self) -> usize;
+    fn reduce(&self) -> &[F];
+}
