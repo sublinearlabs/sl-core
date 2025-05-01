60e73057c44b26f8f9185c7e392de80e3067a612
Ginika-Chinonso <ginika.chinonso.francis@gmail.com>
2025-05-01 20:28:11 +0100

    refac: use fields for multilinear extension trait

--- poly/src/lib.rs
+++ poly/src/lib.rs
@@ -1,16 +1,47 @@
+use p3_field::{ExtensionField, Field};
+
 pub mod mle;
 pub mod vpoly;
 
+#[derive(Debug, Clone, PartialEq, Copy)]
+pub enum Fields<F: Field, E: ExtensionField<F>> {
+    Base(F),
+    Extension(E),
+}
+
+impl<F: Field, E: ExtensionField<F>> Fields<F, E> {
+    pub fn to_base_field(&self) -> Option<F> {
+        match self {
+            Fields::Base(val) => Some(*val),
+            Fields::Extension(_) => panic!("Cant convert extension field to base field"),
+        }
+    }
+
+    pub fn to_extension_field(&self) -> E {
+        match self {
+            Fields::Base(val) => E::from_base(*val),
+            Fields::Extension(val) => *val,
+        }
+    }
+
+    pub fn is_base_field(&self) -> bool {
+        match self {
+            Fields::Base(_) => true,
+            _ => false,
+        }
+    }
+}
+
 /// Multilinear Extension Trait
-pub trait MultilinearExtension<F> {
+pub trait MultilinearExtension<F: Field, E: ExtensionField<F>> {
     /// Fix all variables
-    fn evaluate(&self, point: &[F]) -> F;
+    fn evaluate(&self, point: &[Fields<F, E>]) -> Fields<F, E>;
     /// Partially fix variables starting from the first
-    fn partial_evaluate(&self, point: &[F]) -> Self;
+    fn partial_evaluate(&self, point: &[Fields<F, E>]) -> Self;
     /// Returns the max variable degree
     fn max_degree(&self) -> usize;
     /// Returns the sum of evaluations over the boolean hypercube
-    fn sum_over_hypercube(&self) -> F;
+    fn sum_over_hypercube(&self) -> Fields<F, E>;
     /// Returns the number of variables of the polynomial
     fn num_vars(&self) -> usize;
     /// Converts the polynomial to bytes
