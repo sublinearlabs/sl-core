c9510e150fd8b4b9c996437c27179dfa32d671cd
developeruche <developeruche@gmail.com>
2025-05-02 10:35:44 +0100

    bench re-org

--- poly/benches/barycentric_evaluation_benchmark.rs
+++ poly/benches/barycentric_evaluation_benchmark.rs
@@ -7,31 +7,31 @@ type F = Mersenne31;
 type E = BinomialExtensionField<Mersenne31, 3>;
 
 fn barycentric_evaluation_benchmark(c: &mut Criterion) {
-    let poly_evaluation = vec![
-        Fields::Base(F::new(428910)),
-        Fields::Base(F::new(17234)),
-        Fields::Base(F::new(59102)),
-        Fields::Base(F::new(3679)),
-        Fields::Base(F::new(28465)),
-        Fields::Base(F::new(51007)),
-        Fields::Base(F::new(12398)),
-        Fields::Base(F::new(64020)),
-        Fields::Base(F::new(37561)),
-        Fields::Base(F::new(8946)),
-        Fields::Base(F::new(23175)),
-        Fields::Base(F::new(49303)),
-        Fields::Base(F::new(61428)),
-        Fields::Base(F::new(5916)),
-        Fields::Base(F::new(33782)),
-        Fields::Base(F::new(19054)),
-        Fields::Base(F::new(55629)),
-        Fields::Base(F::new(41963)),
-        Fields::Base(F::new(2847)),
-        Fields::Base(F::new(60215)),
-    ];
-
     c.bench_function("barycentric_evaluation", |b| {
         b.iter(|| {
+            let poly_evaluation = vec![
+                Fields::Base(F::new(428910)),
+                Fields::Base(F::new(17234)),
+                Fields::Base(F::new(59102)),
+                Fields::Base(F::new(3679)),
+                Fields::Base(F::new(28465)),
+                Fields::Base(F::new(51007)),
+                Fields::Base(F::new(12398)),
+                Fields::Base(F::new(64020)),
+                Fields::Base(F::new(37561)),
+                Fields::Base(F::new(8946)),
+                Fields::Base(F::new(23175)),
+                Fields::Base(F::new(49303)),
+                Fields::Base(F::new(61428)),
+                Fields::Base(F::new(5916)),
+                Fields::Base(F::new(33782)),
+                Fields::Base(F::new(19054)),
+                Fields::Base(F::new(55629)),
+                Fields::Base(F::new(41963)),
+                Fields::Base(F::new(2847)),
+                Fields::Base(F::new(60215)),
+            ];
+
             black_box(barycentric_evaluation::<F, E>(
                 &poly_evaluation,
                 &Fields::Base(F::new(10)),
