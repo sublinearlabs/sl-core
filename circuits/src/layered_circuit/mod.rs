9ae7f452e73f95649b69b057d45b52118afd5d3f
developeruche <developeruche@gmail.com>
2025-05-05 11:27:24 +0100

    done with primitives

---
+++ circuits/src/layered_circuit/mod.rs
@@ -0,0 +1,5 @@
+//! An implemenation for a the Layered arithementic circuit, built targetting GKR, Libra protocol
+//! This is not an IR for Virgo at this moment.
+pub mod primitives;
+
+pub struct LayeredCircuit {}
