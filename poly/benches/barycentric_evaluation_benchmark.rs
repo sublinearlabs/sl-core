37fa4a6116869239c2ba1c790f1086668cdeb99e
Wisdom Ogwu <iammadab@gmail.com>
2025-05-06 10:16:47 +0100

    uncomment barycentric bench

--- poly/benches/barycentric_evaluation_benchmark.rs
+++ poly/benches/barycentric_evaluation_benchmark.rs
@@ -1,43 +1,43 @@
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use p3_field::extension::BinomialExtensionField;
 use p3_mersenne_31::Mersenne31;
-use poly::{Fields, utils::barycentric_evaluation};
+use poly::{utils::barycentric_evaluation, Fields};
 
 type F = Mersenne31;
 type E = BinomialExtensionField<Mersenne31, 3>;
 
 fn barycentric_evaluation_benchmark(c: &mut Criterion) {
-    //c.bench_function("barycentric_evaluation", |b| {
-    //    b.iter(|| {
-    //        let poly_evaluation = vec![
-    //            Fields::Base(F::new(428910)),
-    //            Fields::Base(F::new(17234)),
-    //            Fields::Base(F::new(59102)),
-    //            Fields::Base(F::new(3679)),
-    //            Fields::Base(F::new(28465)),
-    //            Fields::Base(F::new(51007)),
-    //            Fields::Base(F::new(12398)),
-    //            Fields::Base(F::new(64020)),
-    //            Fields::Base(F::new(37561)),
-    //            Fields::Base(F::new(8946)),
-    //            Fields::Base(F::new(23175)),
-    //            Fields::Base(F::new(49303)),
-    //            Fields::Base(F::new(61428)),
-    //            Fields::Base(F::new(5916)),
-    //            Fields::Base(F::new(33782)),
-    //            Fields::Base(F::new(19054)),
-    //            Fields::Base(F::new(55629)),
-    //            Fields::Base(F::new(41963)),
-    //            Fields::Base(F::new(2847)),
-    //            Fields::Base(F::new(60215)),
-    //        ];
-    //
-    //        black_box(barycentric_evaluation::<F, E>(
-    //            &poly_evaluation,
-    //            &Fields::Base(F::new(10)),
-    //        ))
-    //    })
-    //});
+    c.bench_function("barycentric_evaluation", |b| {
+        b.iter(|| {
+            let poly_evaluation = vec![
+                Fields::Base(F::new(428910)),
+                Fields::Base(F::new(17234)),
+                Fields::Base(F::new(59102)),
+                Fields::Base(F::new(3679)),
+                Fields::Base(F::new(28465)),
+                Fields::Base(F::new(51007)),
+                Fields::Base(F::new(12398)),
+                Fields::Base(F::new(64020)),
+                Fields::Base(F::new(37561)),
+                Fields::Base(F::new(8946)),
+                Fields::Base(F::new(23175)),
+                Fields::Base(F::new(49303)),
+                Fields::Base(F::new(61428)),
+                Fields::Base(F::new(5916)),
+                Fields::Base(F::new(33782)),
+                Fields::Base(F::new(19054)),
+                Fields::Base(F::new(55629)),
+                Fields::Base(F::new(41963)),
+                Fields::Base(F::new(2847)),
+                Fields::Base(F::new(60215)),
+            ];
+
+            black_box(barycentric_evaluation::<F, E>(
+                &poly_evaluation,
+                &Fields::Base(F::new(10)),
+            ))
+        })
+    });
 }
 
 criterion_group!(benches, barycentric_evaluation_benchmark,);
