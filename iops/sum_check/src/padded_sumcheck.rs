89898e42ba6485014afeb488f00f615044de4fbe
Wisdom Ogwu <iammadab@gmail.com>
2025-06-12 18:37:23 +0100

    implement eval for padded sumcheck

--- iops/sum_check/src/padded_sumcheck.rs
+++ iops/sum_check/src/padded_sumcheck.rs
@@ -1,6 +1,33 @@
 use crate::sumcheckable::Sumcheckable;
+use p3_field::{ExtensionField, Field};
 
 struct PaddedSumcheck<S> {
     inner: S,
     pad_count: usize,
+}
+
+impl<F: Field, E: ExtensionField<F>, S: Sumcheckable<F, E>> Sumcheckable<F, E>
+    for PaddedSumcheck<S>
+{
+    fn no_of_rounds(&self) -> usize {
+        self.inner.no_of_rounds() + self.pad_count
+    }
+
+    fn eval(&self, point: &[poly::Fields<F, E>]) -> poly::Fields<F, E> {
+        assert!(point.len() == self.no_of_rounds());
+        self.inner.eval(&point[..self.inner.no_of_rounds()])
+            * point[self.inner.no_of_rounds()..].iter().cloned().product()
+    }
+
+    fn round_message(&self) -> Vec<poly::Fields<F, E>> {
+        todo!()
+    }
+
+    fn receive_challenge(&mut self, challenge: &poly::Fields<F, E>) {
+        todo!()
+    }
+
+    fn commit(&self, transcript: &mut transcript::Transcript<F, E>) {
+        todo!()
+    }
 }
--- iops/sum_check/src/sumcheckable.rs
+++ iops/sum_check/src/sumcheckable.rs
@@ -1,4 +1,6 @@
 use p3_field::{ExtensionField, Field, PrimeField32};
+use poly::mle::MultilinearPoly;
+use poly::vpoly::VPoly;
 use poly::{Fields, MultilinearExtension};
 use transcript::Transcript;
 
@@ -20,32 +22,69 @@ pub trait Sumcheckable<F: Field, E: ExtensionField<F>> {
     fn commit(&self, transcript: &mut Transcript<F, E>);
 }
 
-impl<F, E, MLE> Sumcheckable<F, E> for MLE
-where
-    F: Field + PrimeField32,
-    E: ExtensionField<F>,
-    MLE: MultilinearExtension<F, E>,
-{
-    fn no_of_rounds(&self) -> usize {
-        self.num_vars()
-    }
+//impl<F, E, MLE> Sumcheckable<F, E> for MLE
+//where
+//    F: Field + PrimeField32,
+//    E: ExtensionField<F>,
+//    MLE: MultilinearExtension<F, E>,
+//{
+//    fn no_of_rounds(&self) -> usize {
+//        self.num_vars()
+//    }
+//
+//    fn eval(&self, point: &[Fields<F, E>]) -> Fields<F, E> {
+//        self.evaluate(point)
+//    }
+//
+//    fn commit(&self, transcript: &mut Transcript<F, E>) {
+//        self.commit_to_transcript(transcript);
+//    }
+//
+//    fn receive_challenge(&mut self, challenge: &Fields<F, E>) {
+//        *self = self.partial_evaluate(&[*challenge]);
+//    }
+//
+//    fn round_message(&self) -> Vec<Fields<F, E>> {
+//        (0..=self.max_degree())
+//            .map(|p| Fields::Extension(E::from_canonical_usize(p)))
+//            .map(|p| self.partial_evaluate(&[p]).sum_over_hypercube())
+//            .collect()
+//    }
+//}
 
-    fn eval(&self, point: &[Fields<F, E>]) -> Fields<F, E> {
-        self.evaluate(point)
-    }
+macro_rules! impl_sumcheckable_for_mle {
+    ($type:ty) => {
+        impl<F, E> Sumcheckable<F, E> for $type
+        where
+            F: Field + PrimeField32,
+            E: ExtensionField<F>,
+            $type: MultilinearExtension<F, E>,
+        {
+            fn no_of_rounds(&self) -> usize {
+                self.num_vars()
+            }
 
-    fn commit(&self, transcript: &mut Transcript<F, E>) {
-        self.commit_to_transcript(transcript);
-    }
+            fn eval(&self, point: &[Fields<F, E>]) -> Fields<F, E> {
+                self.evaluate(point)
+            }
 
-    fn receive_challenge(&mut self, challenge: &Fields<F, E>) {
-        *self = self.partial_evaluate(&[*challenge]);
-    }
+            fn commit(&self, transcript: &mut Transcript<F, E>) {
+                self.commit_to_transcript(transcript);
+            }
 
-    fn round_message(&self) -> Vec<Fields<F, E>> {
-        (0..=self.max_degree())
-            .map(|p| Fields::Extension(E::from_canonical_usize(p)))
-            .map(|p| self.partial_evaluate(&[p]).sum_over_hypercube())
-            .collect()
-    }
+            fn receive_challenge(&mut self, challenge: &Fields<F, E>) {
+                *self = self.partial_evaluate(&[*challenge]);
+            }
+
+            fn round_message(&self) -> Vec<Fields<F, E>> {
+                (0..=self.max_degree())
+                    .map(|p| Fields::Extension(E::from_canonical_usize(p)))
+                    .map(|p| self.partial_evaluate(&[p]).sum_over_hypercube())
+                    .collect()
+            }
+        }
+    };
 }
+
+impl_sumcheckable_for_mle!(MultilinearPoly<F, E>);
+impl_sumcheckable_for_mle!(VPoly<F, E>);
